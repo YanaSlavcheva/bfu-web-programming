62ee0a89d71e4ab4744e1834cd6223aa40450c1b
YanaSlavcheva <yana.slavcheva@gmail.com>
2021-01-13 13:34:27 +0200

    Renamed connect.php to db-connet.php; deleted some comments

--- load_construction_sites.php
+++ load_construction_sites.php
@@ -1,5 +1,5 @@
 <?php
-    include "connect.php";
+    include "db-connect.php";
 
     $sql_get_all_construction_sites = "SELECT `id`, `name`, `address`, `floors_count`, `apartments_count`, `exterior_plaster`, `interior_plaster`, `contractor`, `investor`, `city`, `country` FROM `construction-sites` WHERE 1 ";
 
@@ -16,19 +16,11 @@
         }
     }
 
-    // echo $sql_get_all_construction_sites;
-    
-    // echo gettype($dbConn);
-    // echo gettype($sql_get_all_construction_sites);
-
     $all_construction_sites = mysqli_query($dbConn, $sql_get_all_construction_sites);
-    // echo gettype($all_construction_sites);
-    // var_dump($all_construction_sites);
     if (!$all_construction_sites) {
         echo "<h2>Няма въведени строителни обекти</h2>";
         return;
-    }
-    else {
+    } else {
         echo "<p>Тук ще е таблицата</p>";
         $all_construction_sites_count = mysqli_num_rows($all_construction_sites);
         echo ($all_construction_sites_count);
