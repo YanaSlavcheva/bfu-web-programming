93de36f96a0da8cf8a1f1b7cebf04f918ac69fb6
YanaSlavcheva <yana.slavcheva@gmail.com>
2021-01-14 11:07:21 +0200

    FE validation finished

--- construction_site_add_form.php
+++ construction_site_add_form.php
@@ -3,8 +3,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Добавяне на строителен обект</title>
     <script language="javascript">
-        function validateString (field, fieldLabel, minLength, maxLength) {
-            if (field.value == "")
+        function validateString (field, fieldLabel, minLength, maxLength, regex, regexDescription) {
+            if (field.value.trim() == "")
             {
                 alert (`Моля, въведете ${fieldLabel}!`);
                 field.focus();
@@ -17,8 +17,8 @@
                 return false;
             }
             
-            if (!(/^[0-9A-Za-z_\u0400-\u04FF]*$/.test(field.value))) {
-                alert ('Моля, използвайте само букви, цифри и долно тире!');
+            if (!(regex.test(field.value))) {
+                alert (`Моля, използвайте само ${regexDescription}!`);
                 field.focus();
                 return false;
             }
@@ -27,21 +27,20 @@
         }
 
         function validateNumber(field, fieldLabel, minValue, maxValue) {
-            console.log(field.value);
-            if (field.value == "")
+            if (!field.value)
             {
                 alert (`Моля, въведете ${fieldLabel}!`);
                 field.focus();
                 return false;
             }
 
-            if (typeof field.value !== 'number') {
-                alert (`Моля, въведете число за ${fieldLabel}!`);
+            if (parseInt(field.value) === NaN) {
+                alert (`Моля, въведете число за ${fieldLabel}! ${parseInt(field.value)}`);
                 field.focus();
                 return false;
             }
 
-            if (field.value < minValue || field.value > maxLength) {
+            if (field.value < minValue || field.value > maxValue) {
                 alert (`Моля, за ${fieldLabel} въведете число между ${minValue} и ${maxValue}!`);
                 field.focus();
                 return false;
@@ -51,65 +50,28 @@
         }
 
         function validateForm (form) {
-            // TODO: add client side validation here
-            // name - existing, length between, characters
-            // address- same
-            // number of floors - is number, range
-            // number of apartments - same
-            // изпълнител - existing, length, chars
-            // investor - same
-            // city - same
-            // country - same
+            const nameRegex = /^[0-9A-Za-z\u0400-\u04FF-\s]*$/;
+            const nameRegexDescription = "букви, цифри, интервал и тире";
+            const addressRegex = /^[0-9A-Za-z.,_\u0400-\u04FF-\s]*$/;
+            const addressRegexDescription = "букви, цифри, точка, запетая, тире, интервал и долно тире";
 
-            const isFormValid =
-            // validateString(form.name, "Име", 3, 50)
-                // && validateString(form.address, "Адрес", 3, 200)
-                // && 
-                validateNumber(form.floors_count, "Брой етажи", 0, 100)
-                // && validateNumber(form.apartments_count, 0, 1000)
-                // && validateNumber(form.exterior_plaster, -1, 2)
-                // && validateNumber(form.interior_plaster, -1, 2)
-                // && validateString(form.contractor, "Изпълнител", 3, 100)
-                // && validateString(form.investor, "Инвеститор", 3, 100)
-                // && validateString(form.city, "Град", 3, 50)
-                // && validateString(form.country, "Държава", 3, 50);
+            const isFormValid = validateString(form.name, "Име", 3, 50, nameRegex, nameRegexDescription)
+                && validateString(form.address, "Адрес", 3, 200, addressRegex, addressRegexDescription)
+                && validateNumber(form.floors_count, "Брой етажи", 0, 100)
+                && validateNumber(form.apartments_count, "Брой апартаменти", 0, 1000)
+                && validateString(form.contractor, "Изпълнител", 3, 100, nameRegex, nameRegexDescription)
+                && validateString(form.investor, "Инвеститор", 3, 100, nameRegex, nameRegexDescription)
+                && validateString(form.city, "Град", 3, 50, nameRegex, nameRegexDescription)
+                && validateString(form.country, "Държава", 3, 50, nameRegex, nameRegexDescription);
 
-            return false;
             return isFormValid;
-            // if (form.name.value=="")
-            // {
-            //     alert ("Моля,въведете име!")
-            //     form.name.focus();
-            //     return false;
-            // }
-            
-            // if (form.name.value.length > 50 || form.name.length < 3) {
-            //     alert ("Дължината на името трябва да бъде между 3 и 50 символа!")
-            //     form.name.focus();
-            //     return false;
-            // }
-            
-            // if (!(/^\w+$/.test(form.name.value))) {
-            //     alert ("Моля, използвайте само букви, цифри и долно тире!")
-            //     form.name.focus();
-            //     return false;
-            // }
-
-            // if (form.address.value=="")
-            // { alert ("Моля, въведете адрес!")
-            //     form.address.focus()
-            //     return false
-            // }
-
-            // return false;
         }
     </script>
 </head>
 <body>
     <h1>Добавяне на строителни обекти</h1>
     <h2>Всички полета са задължителни</h2>
-    <!-- TODO: add as action construction_site_add_save_to_db.php -->
-    <form action="" method="post" onSubmit="return validateForm(this)">
+    <form action="construction_site_add_save_to_db.php" method="post" onSubmit="return validateForm(this)">
         <table>
             <tr>
                 <td>Име</td>
